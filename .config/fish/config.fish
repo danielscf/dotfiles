d00d3bcb1f14367cd1892a8cce9214b1d097368f
danielscf <fdanielsantacruz@gmail.com>
2025-08-12 22:26:29 -0500

    fish transient prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
+  function starship_transient_prompt_func
+    starship module character
+  end
   starship init fish | source
+  enable_transience
+  
   zoxide init fish | source
 
   # Aliases
